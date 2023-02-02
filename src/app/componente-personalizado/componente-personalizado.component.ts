6eb7a23430982ffbbbff908e315026499c673ad9
Marcos Vinicius <contato.marcoscandido@gmail.com>
2023-02-02 17:36:36 -0300

    decorador input

---
+++ src/app/componente-personalizado/componente-personalizado.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-personalizado',
+  templateUrl: './componente-personalizado.component.html',
+  styleUrls: ['./componente-personalizado.component.css']
+})
+export class ComponentePersonalizadoComponent {
+
+  
+   @Input() nome  = " "
+   @Input() sobrenome  = " "
+
+
+
+}

6e082b9ad5fbbf014ee6d1b3fd73449f1e0af496
Marcos Vinicius <contato.marcoscandido@gmail.com>
2023-02-27 16:54:56 -0300

    ciclo de vida OnInit e Onchanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { ComponentPaiComponent } from './component-pai/component-pai.component';
 import { ComponenteFilhoComponent } from './componente-filho/componente-filho.component';
 import { ExemploServicos1Component } from './exemplo-servicos1/exemplo-servicos1.component';
 import { ExemploServicos2Component } from './exemplo-servicos2/exemplo-servicos2.component';
+import { CicloDeVidaComponent } from './ciclo-de-vida/ciclo-de-vida.component';
+import { CicloDeVidaPaiComponent } from './ciclo-de-vida-pai/ciclo-de-vida-pai.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,9 @@ import { ExemploServicos2Component } from './exemplo-servicos2/exemplo-servicos2
     ComponentPaiComponent,
     ComponenteFilhoComponent,
     ExemploServicos1Component,
-    ExemploServicos2Component
+    ExemploServicos2Component,
+    CicloDeVidaComponent,
+    CicloDeVidaPaiComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ciclo-de-vida/ciclo-de-vida.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo-de-vida',
+  templateUrl: './ciclo-de-vida.component.html',
+  styleUrls: ['./ciclo-de-vida.component.css']
+})
+export class CicloDeVidaComponent implements OnInit , OnChanges{
+
+  @Input() texto = "";
+
+   horario = new Date();
+
+   timer: any = null!;
+
+  ngOnInit(): void {
+
+    console.log("O evento OnInit disparou");
+    this.timer = setInterval(()=>this.horario= new Date () , 1000 );
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      
+    console.log(changes);
+  }
+
+}
